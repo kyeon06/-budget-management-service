ee8803fbdfe495725d0cf0d7083a3ff4609cbe2b
kyeon06 <jkyeon06@gmail.com>
2023-11-10 22:10:08 +0900

    :sparkles: feat : swagger setting

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Budget Management App",
+        default_version="v1",
+        description="사용자가 예산과 지출을 관리할 수 있는 서비스입니다.",
+        contact=openapi.Contact(email="jkyeon06@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     # admin
@@ -10,6 +24,7 @@ urlpatterns = [
     # api
 
     # swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 
 ]
 
