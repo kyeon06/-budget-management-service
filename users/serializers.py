7f7ce6f03fb71ff51610d55a9f4ba29b1637e07a
kyeon06 <jkyeon06@gmail.com>
2023-11-12 23:52:09 +0900

    :sparkles: feat : 회원가입 API

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class SignupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password'
+        ]
+
+    def create(self, validated_data):
+        username = validated_data.get('username')
+        password = validated_data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError("유저가 존재합니다. 다른 계정명을 사용해주세요.")
+        
+        user = User.objects.create_user(username=username, password=password)
+
+        return user
+
+class SignupOutputSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    message = serializers.CharField()
