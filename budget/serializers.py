856143233566c08f49b7ef70d67b55d2d6d0171e
kyeon06 <jkyeon06@gmail.com>
2023-11-15 00:27:57 +0900

    :sparkles: feat : 예산 생성 API

--- budget/serializers.py
+++ budget/serializers.py
@@ -3,6 +3,19 @@ from rest_framework import serializers
 from budget.models import Budget
 
 
+class BudgetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Budget
+        fields = [
+            'user',
+            'category',
+            'money',
+            'start_date',
+            'end_date'
+        ]
+
+
 class BudgetListSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
     category = serializers.StringRelatedField()
@@ -16,3 +29,10 @@ class BudgetListSerializer(serializers.ModelSerializer):
             'start_date',
             'end_date'
         ]
+
+
+class BudgetCreateSerializer(serializers.Serializer):
+    category = serializers.CharField()
+    money = serializers.IntegerField()
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
--- budget/views.py
+++ budget/views.py
@@ -5,7 +5,8 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
 from budget.models import Budget
-from budget.serializers import BudgetListSerializer
+from budget.serializers import BudgetCreateSerializer, BudgetListSerializer, BudgetSerializer
+from categories.models import Category
 
 
 # api/v1/budget/
@@ -13,6 +14,9 @@ class BudgetAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        """
+        예산 목록
+        """
         user = request.user
 
         budget_list = Budget.objects.filter(user=user)
@@ -21,7 +25,38 @@ class BudgetAPIView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    
+    @swagger_auto_schema(request_body=BudgetCreateSerializer,
+                         responses={status.HTTP_201_CREATED : BudgetListSerializer})
     def post(self, request):
-        pass
+        """
+        예산 생성
+        """
+        user = request.user
+        budget_data = request.data
+
+        required_field = (not budget_data['money']) or (not budget_data['start_date']) or (not budget_data['end_date'])
+        if required_field:
+            raise ValueError("예산 기간과 금액을 입력해주세요.")
+        
+        category = budget_data['category']
+        if category:
+            budget_data['category'] = Category.objects.get(name=category).id
+        
+        budget_data["user"] = user.id
+
+        serializer = BudgetSerializer(data=budget_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+
+
+
 
 
