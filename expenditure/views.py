38e66e366d14b266fba6c15740e6929275d45e96
kyeon06 <jkyeon06@gmail.com>
2023-11-16 16:26:24 +0900

    :sparkles: feat : 지출 내역 수정 API

--- expenditure/views.py
+++ expenditure/views.py
@@ -109,3 +109,35 @@ class ExpenditureDetailAPIView(APIView):
         
         serializer = ExpenditureDetailSerializer(expense_data)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    @swagger_auto_schema(
+            request_body=ExpenditureCreateSerializer,
+            responses={
+                status.HTTP_200_OK : ExpenditureDetailSerializer
+            }
+    )
+    def put(self, request, expenditure_id):
+        user = request.user
+        update_data = request.data
+
+        try:
+            expense_data = Expenditure.objects.get(user=user, id=expenditure_id)
+            category_data = Category.objects.get(name=update_data['category'])
+        except Exception as e :
+            return Response(
+                {
+                    "error_code" : str(e),
+                    "message" : "해당 내역을 찾을 수 없습니다."
+                },
+                status=status.HTTP_404_NOT_FOUND
+                )
+        
+        update_data['category'] = category_data.id
+
+        serializer = ExpenditureSerializer(expense_data, data=update_data, partial=True)
+        if serializer.is_valid():
+            updated_data = serializer.save()
+            return Response(ExpenditureDetailSerializer(updated_data).data, status=status.HTTP_200_OK)
+        
+        return Response({"message" : "내역 수정에 실패하였습니다. 다시 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
